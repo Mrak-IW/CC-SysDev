503bbdb93f15c91797565a5126eedaf3104e3f09
Denis Maslov <d.vi.maslov@gmail.com>
2016-09-29 21:22:01 +0300

    TODO: wait & kill

---
+++ 1. ProcessesAndDaemons/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+
+void forkchild()
+{
+	printf("I'm forked child! My PID is %d\n", (int)getpid());
+}
+
+int main(int argc, char *argv[])
+{
+	pid_t child_pid;
+	printf("The main program process ID is %d\n\n", (int)getpid());
+
+	const char* systemCommand = "ls -lah";
+	printf("New process using system(\"%s\"):\n", systemCommand);
+	system(systemCommand);
+
+
+	child_pid = fork();
+	if(0 != child_pid)
+	{
+		//This is a parent process
+		printf("Child process vas been forked. Its PID is %d\n", child_pid);
+	}
+	else
+	{
+		//This is a child process
+		forkchild();
+		return 0;
+	}
+
+	//spawn() is not implemented in Linux
+
+	const char* execCmd = "/bin/ls";
+	const char* execParam1 = "-lah";
+	printf("New process using execle(\"%s\", \"%s\", %p):\n\t", execCmd, execParam1,  NULL);
+	int ret = execle(execCmd, execParam1, NULL);
+	printf("Вот эта строка у меня не выводится. Интересно, почему?\n");
+	printf("Process exited with code '%d: %s'\n", errno, strerror(errno));
+
+	return 0;
+}
