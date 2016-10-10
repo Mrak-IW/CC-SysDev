a7f625d0b73e6342adf3db1415ef0a6c7f77008b
Denis Maslov <denis.maslov@ostware-services.com>
2016-10-10 15:54:35 +0300

    21_Signals -  RT signals done

--- 21_Signals/21_Signals/main.c
+++ 21_Signals/21_Signals/main.c
@@ -9,12 +9,13 @@
 void parentSigHandler(int signum, siginfo_t *siginfo, void * context)
 {
 	union sigval value = siginfo->si_value;
+	char* sigtype = (signum == SIGUSR1 ? "UNIX" : "RT");
 
-	printf("Parent: SIG:%d received\n", signum);
-	printf("Parent: VAL:%d received\n", value.sival_int);
-	/*printf("Parent: SI_CODE:%d received\n", siginfo->si_code);
-	printf("Parent: SI_ERRNO:%d received\n", siginfo->si_errno);
-	printf("Parent: SI_SIGNO:%d received\n", siginfo->si_signo);*/
+	printf("Parent:\tSIG:%s received\n", sigtype);
+	printf("\tVAL:%d\n", value.sival_int);
+	/*printf("Parent:\tSI_CODE:%d received\n", siginfo->si_code);
+	printf("Parent:\tSI_ERRNO:%d received\n", siginfo->si_errno);
+	printf("Parent:\tSI_SIGNO:%d received\n", siginfo->si_signo);*/
 }
 
 void parentProcess(pid_t child_pid)
@@ -29,29 +30,31 @@ void parentProcess(pid_t child_pid)
 	action.sa_flags = SA_SIGINFO;
 	sigemptyset(&(action.sa_mask));
 	sigaction(SIGUSR1, &action, NULL);
+	sigaction(SIGRTMIN + 1, &action, NULL);
 
-	printf("Parent: My PID is %d\n", getpid());
-	printf("Parent: Child's PID is %d\n", child_pid);
-	printf("Parent: Waiting for a signal USR1\n");
+	printf("Parent:\tMy PID is %d\n", getpid());
+	printf("Parent:\tChild's PID is %d\n", child_pid);
+	printf("Parent:\tWaiting for a signal USR1\n");
 
-	/* Дожидаемся завершения дочернего процесса. */
+	/* Waiting for child process ending */
 	int status;
 	while(1)
 	{
-		waitpid(child_pid, &status, 0);
+		wait(&status);
 		if(WIFEXITED(status))
 		{
 			break;
 		}
 	}
-	printf("Parent: Child process is over\n");
+	//sleep(5);
+	printf("Parent:\tI'm tired. I'm going away.\n");
 }
 
 void childProcess(pid_t parent_pid)
 {
-	printf("Child: My PID is %d\n", getpid());
-	printf("Child: Parent's PID is %d\n", parent_pid);
-	kill(parent_pid, SIGUSR1);
+	printf("Child:\tMy PID is %d\n", getpid());
+	printf("Child:\tParent's PID is %d\n", parent_pid);
+	//kill(parent_pid, SIGUSR1);
 	union sigval value;
 	value.sival_int = 0;
 	int i;
@@ -62,6 +65,13 @@ void childProcess(pid_t parent_pid)
 		sigqueue(parent_pid, SIGUSR1, value);
 		//usleep(500000);
 		//usleep(1);
+	}
+
+	for(i = 0; i < 5; i++)
+	{
+		value.sival_int = SIGRTMIN + i;
+		printf("Child: Sending RT message %d to parent.\n", SIGRTMIN + i);
+		sigqueue(parent_pid, SIGRTMIN + 1, value);
 	}
 }
 
